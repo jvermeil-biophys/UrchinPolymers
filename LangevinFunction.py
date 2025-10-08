6a22e2a3f720d6a96dd259b78915f0d1aba88ae7
JosephV <josephvermeil@gmail.com>
2025-10-08 12:44:17 +0200

    Langevin 2.2

--- LangevinFunction.py
+++ LangevinFunction.py
@@ -10,7 +10,7 @@ Created on Mon Oct  6 14:41:38 2025
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mpmath import coth
+# from mpmath import coth
 
 mu0 = 4*np.pi*1e-7 # Permeabilite magnetique du vide [µ0] - H/m (Henry/metre)
 kB = 1.380649e-23 # Constante de Boltzman - J/K (Joule/Kelvin)
@@ -93,6 +93,79 @@ ax.set_ylabel('Bead Magnetization (A/m)', color = 'g')
 plt.tight_layout()
 plt.show()
 
+# %% Tests with Langevin Function
+
+def L1_A(a1, a2, X):
+    mask = (np.abs(X) < 0.01)
+    res = np.zeros_like(X)
+    res[mask]= a1*np.tanh(a2*X[mask]/3)
+    res[~mask] = a1*((1/np.tanh(a2*X[~mask])) - (1/(a2*X[~mask])))
+    return(res)
+
+
+XX = np.linspace(-5, 5, 1000)
+YYref = L1_A(1, 1, XX)
+
+fig, ax = plt.subplots(1, 1)
+
+a1 = 1
+for a2 in [0.8, 0.9, 1, 1.1, 1.2]:
+    ax.plot(XX, L1_A(a1, a2, XX)/YYref)
+    
+ax.grid()
+fig.tight_layout()
+plt.show()
+
+
+# fig, ax = plt.subplots(1, 1)
+
+# a = 1
+# for b in [0.25, 0.5, 1, 2, 4]:
+#     ax.plot(XX, L1_A(a, b, XX)/YYref)
+
+# ax.grid()
+
+# fig.tight_layout()
+# plt.show()
+
+# %% Computation with the paper values
+
+ms_p = 336e3
+D_p = 7.0e-9
+R_p = D_p/2
+V_p = (4/3)*np.pi*R_p**3
+
+rho_b = 1.7
+phi_b = 0.119
+
+M0 = phi_b * ms_p / (rho_b*1000)
+
+# Chi = phi_b*ms_p * (V_p*ms_p) * (1/(3*kB*T)) * (1/rho_b*1000)
+Chi = M0 * (V_p*ms_p) * (1/(3*kB*T)) * (mu0/2)
+
+A1 = 10.8
+A2 = 3*54e-5/(A1*mu0)
+B1 = 19.6
+B2 = 3*102e-5/(B1*mu0)
+C1 = 23.5
+C2 = 3*81e-5/(C1*mu0)
+
+def L1_A(a1, a2, X):
+    mask = (np.abs(X) < 0.001)
+    res = np.zeros_like(X)
+    res[mask]= a1*np.tanh(a2*X[mask]/3)
+    res[~mask] = a1*((1/np.tanh(a2*X[~mask])) - (1/(a2*X[~mask])))
+    return(res)
+
+XX = np.linspace(-1, 1, 1000)
+# XX = np.linspace(-0.015, 0.015, 1000)
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(XX, L1_A(A1, A2, XX))
+    
+ax.grid()
+fig.tight_layout()
+plt.show()
 
 
 # %% Plots
